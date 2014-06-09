731da621ca2af49010db8f9150534ae411d05e7a
Greg Guthe <gguthe@rjmetrics.com>
2014-06-08 20:20:43 -0400

    Add Import API support

---
+++ rjmetrics/client.py
@@ -0,0 +1,103 @@
+
+import sys
+
+import requests
+
+
+if sys.version < '3':
+    _STR_TYPES = set([str, unicode])
+    from itertools import izip_longest as zip_longest
+else:
+    _STR_TYPES = set([str])
+    from itertools import zip_longest
+
+
+def grouper(iterable, n, fillvalue=None):
+    """
+    Collect data into fixed-length chunks or blocks.
+
+    grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+
+    From: https://docs.python.org/2/library/itertools.html#recipes
+    """
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
+
+class Client(object):
+
+    API_BASE = 'https://connect.rjmetrics.com/v2'
+    SANDBOX_BASE = 'https://sandbox-connect.rjmetrics.com/v2'
+
+    _AUTH_TEST_DATA = [{"keys": ["id"], "id": 1}]
+    _MAX_RETRIES = 3
+    _BATCH_SIZE = 100  # items per request
+
+    def __init__(self, client_id, api_key):
+        """
+        Creates a new Python client to the RJMetrics API.
+        """
+
+        if type(client_id) != int:
+            raise ValueError("Client ID '%s' must be an integer." % client_id)
+
+        if type(api_key) not in _STR_TYPES:
+            raise ValueError("Import API key '%s' must be a string." % api_key)
+
+        self.client_id = client_id
+        self.api_key = api_key
+
+    @staticmethod
+    def _url(base_url, client_id, table_name, api_key):
+
+        return "%s/client/%s/table/%s/data?apikey=%s" % \
+          (base_url, client_id, table_name, api_key)
+
+    def authenticate(self):
+        """
+        Sends a test data point to the sandbox to check API_KEY credentials
+        and returns True if the test data is accepted (False otherwise).
+        """
+        posts = self.push_data("test", self._AUTH_TEST_DATA, self.SANDBOX_BASE)
+        return (posts[0].status_code == 201)
+
+    def push_data(self, table_name, data, base_url=None):
+        """
+        Pushes data as a list of dicts to the Import API.
+
+        Each data point should have a list of keys. For example:
+
+        [{"keys": ["id"], "id": 1}]
+        """
+        if type(table_name) not in _STR_TYPES:
+            raise ValueError("Table name '%s' must be a string." % table_name)
+
+        if type(data) != list:
+            raise ValueError("Data '%s' must be a list." % data)
+
+        if len(data) == 0:
+            return []
+
+        if base_url is None:
+            base_url = self.API_BASE
+
+        url = Client._url(base_url, self.client_id, table_name, self.api_key)
+
+        posts = []
+        for batch in grouper(data, Client._BATCH_SIZE):
+            batch = [x for x in batch if x != None]
+            posts.append(requests.post(url, data=batch))
+
+        return posts
+
+    def stream_data():
+        """
+        Pushes data from an iterable.
+        """
+        raise NotImplementedError()
+
+    def fetch_data():
+        """
+        Fetches a table from the Export API.
+        """
+        raise NotImplementedError()
---
+++ rjmetrics/test/test_client.py
@@ -0,0 +1,102 @@
+
+import itertools
+import sys
+import unittest
+
+import mock
+
+from rjmetrics.client import Client
+
+
+API_KEY = 'test-api-key'
+CLIENT_ID = 12
+
+
+class TestClientInit(unittest.TestCase):
+
+    def test_init_success(self):
+        test_client = Client(CLIENT_ID, API_KEY)
+        self.assertTrue(isinstance(test_client, Client))
+
+        if sys.version < '3':
+            test_client = Client(CLIENT_ID, unicode(API_KEY))
+            self.assertTrue(isinstance(test_client, Client))
+
+    def test_init_requires_int_client_id(self):
+        self.assertRaises(ValueError, Client, str(CLIENT_ID), API_KEY)
+
+    def test_init_requires_str_or_unicode_api_key(self):
+        self.assertRaises(ValueError, Client, CLIENT_ID, 30)
+
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client(CLIENT_ID, API_KEY)
+
+    ## authenticate
+
+    @mock.patch('requests.post')
+    def test_authenticate_succeeds_on_test_data_created(self, mock_post):
+        mock_post.return_value.status_code = 201
+
+        self.assertTrue(self.client.authenticate())
+
+        mock_post.assert_called_once_with(
+            'https://sandbox-connect.rjmetrics.com/v2/'
+            'client/12/table/test/data?apikey=test-api-key',
+            data=Client._AUTH_TEST_DATA)
+
+    @mock.patch('requests.post')
+    def test_authenticate_fails_on_unauthorized(self, mock_post):
+        mock_post.return_value.status_code = 401
+
+        self.assertFalse(self.client.authenticate())
+
+        mock_post.assert_called_once_with(
+            'https://sandbox-connect.rjmetrics.com/v2/'
+            'client/12/table/test/data?apikey=test-api-key',
+            data=Client._AUTH_TEST_DATA)
+
+
+    ## push_data
+
+    def test_push_data_requires_str_or_unicode_tablename(self):
+        self.assertRaises(ValueError, self.client.push_data, 20, []);
+
+    def test_push_data_requires_list_data(self):
+        self.assertRaises(ValueError, self.client.push_data, '20', set());
+
+    def test_push_data_no_data_returns_empty_list(self):
+        self.assertEqual(self.client.push_data('20', []), []);
+
+    @mock.patch('requests.post')
+    def test_push_data_one_batch(self, mock_post):
+        test_data = list(itertools.repeat(Client._AUTH_TEST_DATA[0], 2))
+
+        mock_post.return_value.status_code = 201
+
+        self.client.push_data('test', test_data, Client.SANDBOX_BASE)
+
+        mock_post.assert_called_once_with(
+            'https://sandbox-connect.rjmetrics.com/v2/'
+            'client/12/table/test/data?apikey=test-api-key',
+            data=test_data)
+
+    @mock.patch('requests.post')
+    def test_push_data_many_batches(self, mock_post):
+        # breaks data into BATCH_SIZE batches
+        test_data = [{"keys": ["id"], "id": i} for i in range(144)]
+
+        mock_post.return_value.status_code = 201
+
+        self.client.push_data('test', test_data, Client.SANDBOX_BASE)
+
+        expected_url = 'https://sandbox-connect.rjmetrics.com/v2/' \
+            'client/12/table/test/data?apikey=test-api-key'
+
+        self.assertEqual(mock_post.call_args_list[0],
+                         mock.call(expected_url, data=test_data[0:Client._BATCH_SIZE]))
+
+        self.assertEqual(mock_post.call_args_list[1],
+                         mock.call(expected_url, data=test_data[Client._BATCH_SIZE:]))
