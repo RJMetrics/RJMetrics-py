2134ce1beba70bc0b600906cdb7efbf129dd21dd
Greg Guthe <greg.guthe@gmail.com>
2014-09-03 15:53:27 -0400

    Add examples for the dev portal

---
+++ examples/orders_table.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rjmetrics.client
+
+
+client = rjmetrics.client.Client(your_client_id, your_api_key)
+
+fake_orders = [
+  {"id": 1, "user_id": 1, "value": 58.40,  "sku": "milky-white-suede-shoes"},
+  {"id": 2, "user_id": 1, "value": 23.99,  "sku": "red-button-down-fleece"},
+  {"id": 3, "user_id": 2, "value": 5.00,   "sku": "bottle-o-bubbles"},
+  {"id": 4, "user_id": 3, "value": 120.01, "sku": "zebra-striped-game-boy"},
+  {"id": 5, "user_id": 5, "value": 9.90,   "sku": "kitten-mittons"}
+]
+
+def sync_order(client, order):
+  order["keys"] = ["id"]
+  return client.push_data("orders", [order])[0]
+
+# make sure the client is authenticated before we do anything
+if client.authenticated():
+  for order in fake_orders:
+    # iterate through users and push data
+    response = sync_order(client, order)
+    if response.ok:
+        print "Synced order with id ", order["id"]
+    else
+        print "Failed to sync order with id ", order["id"]
---
+++ examples/users_table.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rjmetrics.client
+
+
+client = rjmetrics.client.Client(your_client_id, your_api_key)
+
+# let's define some fake users
+fake_users = [
+  {"id": 1, "email": "joe@schmo.com", "acquisition_source": "PPC"},
+  {"id": 2, "email": "mike@smith.com", "acquisition_source": "PPC"},
+  {"id": 3, "email": "lorem@ipsum.com", "acquisition_source": "Referral"},
+  {"id": 4, "email": "george@vandelay.com", "acquisition_source": "Organic"},
+  {"id": 5, "email": "larry@google.com", "acquisition_source": "Organic"}
+]
+
+def sync_user(client, user):
+  # `id` is the unique key here, since each user should only
+  # have one record in this table
+  user["keys"] = ["id"]
+  # table named "users"
+  return client.push_data("users", [user])[0]
+
+# make sure the client is authenticated before we do anything
+if client.authenticated():
+  for user in fake_users:
+    # iterate through users and push data
+    response = sync_user(client, user)
+    if response.ok:
+        print "Synced user with id ", user["id"]
+    else
+        print "Failed to sync user with id ", user["id"]
