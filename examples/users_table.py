e12737a819ef28d41c3e3c7ab14b2b43a89b4e70
Greg Guthe <greg.guthe@gmail.com>
2014-09-08 14:29:27 -0400

    Fix tab width to 4 spaces

--- examples/users_table.py
+++ examples/users_table.py
@@ -9,28 +9,28 @@ client = rjmetrics.client.Client(CLIENT_ID, API_KEY)
 
 # let's define some fake users
 fake_users = [
-  {"id": 1, "email": "joe@schmo.com", "acquisition_source": "PPC"},
-  {"id": 2, "email": "mike@smith.com", "acquisition_source": "PPC"},
-  {"id": 3, "email": "lorem@ipsum.com", "acquisition_source": "Referral"},
-  {"id": 4, "email": "george@vandelay.com", "acquisition_source": "Organic"},
-  {"id": 5, "email": "larry@google.com", "acquisition_source": "Organic"}
+    {"id": 1, "email": "joe@schmo.com", "acquisition_source": "PPC"},
+    {"id": 2, "email": "mike@smith.com", "acquisition_source": "PPC"},
+    {"id": 3, "email": "lorem@ipsum.com", "acquisition_source": "Referral"},
+    {"id": 4, "email": "george@vandelay.com", "acquisition_source": "Organic"},
+    {"id": 5, "email": "larry@google.com", "acquisition_source": "Organic"}
 ]
 
 
 def sync_user(client, user):
-  # `id` is the unique key here, since each user should only
-  # have one record in this table
-  user["keys"] = ["id"]
-  # table named "users"
-  return client.push_data("users", [user])[0]
+    # `id` is the unique key here, since each user should only
+    # have one record in this table
+    user["keys"] = ["id"]
+    # table named "users"
+    return client.push_data("users", [user])[0]
 
 
 # make sure the client is authenticated before we do anything
 if client.authenticate():
-  for user in fake_users:
-    # iterate through users and push data
-    response = sync_user(client, user)
-    if response.ok:
-        print "Synced user with id ", user["id"]
-    else:
-        print "Failed to sync user with id ", user["id"]
+    for user in fake_users:
+        # iterate through users and push data
+        response = sync_user(client, user)
+        if response.ok:
+            print "Synced user with id ", user["id"]
+        else:
+            print "Failed to sync user with id ", user["id"]
